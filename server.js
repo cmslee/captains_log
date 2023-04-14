137598b58966022bcc867b83d95a8a3fb2faeaac
Casey <c.m.s.lee0207@gmail.com>
2023-04-14 14:41:16 -0400

    create route

--- server.js
+++ server.js
@@ -8,12 +8,28 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('jsx-view-engine').createEngine());
 
 //*MIDDLEWARE
+app.use(express.urlencoded({extended: false}))
 
 //* ROUTES
 
-//new
+//home
 app.get('/', (req, res) => {
+    res.send('Hello, captain log!')
+})
+
+//new route: form
+app.get('/logs/new', (req, res) => {
     res.render('New')
+})
+
+//create route: POST
+app.post('/logs', (req, res) => {
+    if (req.body.shipIsBroken === 'on'){
+        req.body.shipIsBroken = true;
+    } else {
+        req.body.shipIsBroken = false;
+    }
+    res.send(req.body)
 })
 
 //* LISTEN
